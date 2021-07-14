e57df363457479f2133d6548b0b2a270924aae71
agoetc <tkgw666private@gmail.com>
2021-07-14 20:04:27 +0900

    isToAllの挙動修正

---
+++ src/adapter/dom/getter/AccountDomGetter.ts
@@ -0,0 +1,11 @@
+import { Common } from './Common'
+
+export const AccountDomGetter = {
+  getAccountListFromTo(): HTMLCollection {
+    const AccountListDom: HTMLCollection = document
+      .getElementsByClassName('_cwLTList tooltipList')[2]
+      .getElementsByTagName('li')
+
+    return Common.nullCheck(AccountListDom)
+  },
+}
--- src/adapter/dom/reader/AccountDomReader.ts
+++ src/adapter/dom/reader/AccountDomReader.ts
@@ -1,39 +1,36 @@
 import { Account, AccountId, AccountList } from '../../../domain/Account'
 import { AccountReader } from '../../../domain/reader/AccountReader'
+import { AccountDomGetter } from '../getter/AccountDomGetter'
 
 export const AccountDomReader: AccountReader = {
   /** CanDo **/
   getAccountList(): AccountList {
-    const toAccountListDOM: HTMLCollection = document
-      .getElementsByClassName('_cwLTList tooltipList')[2]
-      .getElementsByTagName('li')
+    const accountListDom = AccountDomGetter.getAccountListFromTo()
+    return PAccountDomReader.buildAccountList(accountListDom)
+  },
+}
 
+const PAccountDomReader = {
+  buildAccountList(accountListDom: HTMLCollection): AccountList {
     const accountList: AccountList = { value: [] }
 
-    for (let i = 0; i < toAccountListDOM.length; i++) {
-      const item: Element = toAccountListDOM[i]
-      if (!PrivateAccountDomReader.isToAll(item)) {
-        const account: Account | null =
-          PrivateAccountDomReader.buildAccount(item)
-        if (account !== null) {
-          accountList.value.push(account)
-        }
+    for (let i = 0; i < accountListDom.length; i++) {
+      const accountDom = <HTMLLIElement>accountListDom[i]
+      if (!PAccountDomReader.isToAll(accountDom)) {
+        const account: Account = PAccountDomReader.buildAccount(accountDom)
+        accountList.value.push(account)
       }
     }
     return accountList
   },
-}
-const PrivateAccountDomReader = {
   /**
    * CanDo
-   * TODO: any
-   * @param accountDOM
    */
-  buildAccount(accountDOM: any): Account | null {
-    const accountId: string | null = accountDOM.dataset.cwuiLtValue
+  buildAccount(accountDOM: HTMLLIElement): Account {
+    const accountId: string | undefined = accountDOM.dataset.cwuiLtValue
     const imagePath: string | null = accountDOM.children[0].getAttribute('src')
     const name: string | null = accountDOM.children[1].textContent
-    if (accountId !== null && imagePath !== null && name !== null) {
+    if (accountId !== undefined && imagePath !== null && name !== null) {
       return {
         accountId: AccountId.fromString(accountId),
         imagePath: imagePath,
@@ -42,16 +39,14 @@ const PrivateAccountDomReader = {
     } else {
       console.log('error')
       console.log(accountId, imagePath, name)
-      // TODO: throw
-      return null
+      throw DOMException
     }
   },
   /**
    * CanDo
-   * TODO: any
-   * @param accountDOM Element
    */
-  isToAll(accountDOM: any): boolean {
-    return Number(accountDOM.dataset.cwuiLtValue) === 0
+  isToAll(accountDOM: HTMLLIElement): boolean {
+    const index: string | undefined = accountDOM.dataset.cwuiLtValue
+    return index !== undefined || index === '0'
   },
 }
