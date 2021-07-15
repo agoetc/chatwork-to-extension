1d2ad68e39f6d4cd9b2f2a97fe3978294f7c47ee
agoetc <tkgw666private@gmail.com>
2021-07-15 22:28:15 +0900

    create delete button

--- src/app/dom/generater/group/dialog/GroupEditDialog.ts
+++ src/app/dom/generater/group/dialog/GroupEditDialog.ts
@@ -4,6 +4,7 @@ import { AddForm } from './AddForm'
 import { SelectBox } from './SelectBox'
 import { CheckTable } from './CheckTable'
 import { AccountList } from '../../../../../domain/Account'
+import { env } from '../../../../../env'
 
 export const GroupEditDialog = {
   generate(
@@ -14,7 +15,7 @@ export const GroupEditDialog = {
     const groupDiv = document.createElement('div')
     groupDiv.appendChild(AddForm.generate(groupList))
     groupDiv.appendChild(SelectBox.generate(groupList))
-    // groupDiv.appendChild(groupListDOMBuilder.deleteButton())
+    groupDiv.appendChild(PGroupEditDialog.deleteButton())
 
     // モーダルに要素を追加している
     const dialog: HTMLDialogElement = PGroupEditDialog.dialog()
@@ -33,5 +34,24 @@ const PGroupEditDialog = {
     const dialog = document.createElement('dialog')
     dialog.id = 'grouping-dialog'
     return dialog
+  },
+  deleteButton(): HTMLSpanElement {
+    const button = document.createElement('button')
+    button.id = env.id.saveButton.button
+    button.className = '_cwDGButton  _cwDGButtonCancel button buttonGray'
+
+    button.textContent = '削除する'
+    button.addEventListener('click', () => {
+      // if (!state.isDefaultSelect) {
+      //   const groupName = document.getElementById(env.id.select.select).value
+      //   this.groupList.deleteGroup(groupName)
+      // }
+    })
+
+    const span = document.createElement('span')
+    span.id = env.id.deleteButton.button
+    span.appendChild(button)
+
+    return span
   },
 }
