b446942c83bea3f6f22353f1e8cca56507da4361
agoetc <tkgw666private@gmail.com>
2021-07-23 00:50:56 +0900

    clear unused import

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,4 @@
 import { Applier } from './app/applier/Applier'
-import { GroupGetter } from './adapter/dom/group/getter/GroupGetter'
-import { GroupStorageRepository } from './adapter/storage/repository/GroupStorageRepository'
-import { GroupInToList } from './app/dom/generater/original/GroupInToList'
-import { ToListDomGetter } from './adapter/dom/original/getter/ToListDomGetter'
-import { Group } from './domain/Group'
-import { AccountList } from './domain/Account'
-import { TextAreaDomGetter } from './adapter/dom/original/getter/TextAreaDomGetter'
 import { ToListObserver } from './app/observer/original/ToListObserver'
 
 window.onload = () => setTimeout(listener, 1000)
