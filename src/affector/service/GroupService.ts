fe9dd1d97de946d7af84d002aa0eb2dae1842d09
agoetc <tkgw666private@gmail.com>
2021-07-23 17:48:52 +0900

    アカウントの重複をなくした

--- src/affector/service/GroupService.ts
+++ src/affector/service/GroupService.ts
@@ -1,7 +1,8 @@
 import { Group, GroupList } from '../../domain/Group'
 import { GroupStorageRepository } from '../../adapter/storage/repository/GroupStorageRepository'
-import { AccountList } from '../../domain/Account'
+import { Account, AccountList } from '../../domain/Account'
 import { SaveGroupRequest } from '../../domain/SaveGroupRequest'
+import { AccountDomReader } from '../../adapter/dom-reader/original/AccountDomReader'
 
 export const GroupService = {
   getGroupList(): Promise<GroupList> {
@@ -32,9 +33,12 @@ const PGroupService = {
   // TODO あんまキレイじゃない
   buildSaveGroup(groupList: GroupList, req: SaveGroupRequest): GroupList {
     const group = PGroupService.findByGroupName(groupList, req.name)
+    const accountListFromToList: AccountList = AccountDomReader.getAccountList()
+
     if (group !== undefined) {
-      const mergedAccountList = AccountList.mergeAccountListRequest(
+      const mergedAccountList = this.mergeAccountListRequest(
         group.accountList,
+        accountListFromToList,
         req.accountList
       )
       const saveGroupList = groupList.value.map((group) => {
@@ -51,5 +55,34 @@ const PGroupService = {
       groupList.value.push(SaveGroupRequest.toGroup(req))
       return groupList
     }
+  },
+  mergeAccountListRequest(
+    savedAccountList: AccountList,
+    accountListFromToList: AccountList,
+    reqAccountList: AccountList
+  ): AccountList {
+    /**
+     * チャット外の人は使い回す
+     * 既にGroupに追加されている人は使い回さない（名前とか変わっている可能性あるので）
+     * @type {Array<Account>}
+     */
+    const reuseAccountListAccount = savedAccountList.value.filter((savedAccount) => {
+      return (
+        this.isOutsider(savedAccount, accountListFromToList) ||
+        !this.isSaved(savedAccount, reqAccountList)
+      )
+    })
+
+    return { value: reuseAccountListAccount.concat(reqAccountList.value) }
+  },
+  isOutsider(savedAccount: Account, accountListFromToList: AccountList): boolean {
+    return !accountListFromToList.value.some(
+      (toAccount) => toAccount.accountId.value === savedAccount.accountId.value
+    )
+  },
+  isSaved(savedAccount: Account, reqAccountList: AccountList): boolean {
+    return reqAccountList.value.some(
+      (reqAccount) => reqAccount.accountId.value === savedAccount.accountId.value
+    )
   },
 }
--- src/domain/Account.ts
+++ src/domain/Account.ts
@@ -1,4 +1,3 @@
-import { AccountDomReader } from '../adapter/dom-reader/original/AccountDomReader'
 import { ListObject, ValueObject } from './Common'
 
 export interface Account {
@@ -19,42 +18,4 @@ export const AccountId = {
 
 export interface AccountList extends ListObject {
   value: Account[]
-}
-
-export const AccountList = {
-  buildByObj(accountListObj: AccountList): AccountList {
-    const accountList: AccountList = { value: [] }
-    Object.keys(accountListObj.value).forEach((key) => {
-      const keyNumber = Number(key)
-      const account: Account = {
-        accountId: accountListObj.value[keyNumber].accountId,
-        imagePath: accountListObj.value[keyNumber].imagePath,
-        name: accountListObj.value[keyNumber].name,
-      }
-      accountList.value.push(account)
-    })
-
-    return accountList
-  },
-  mergeAccountListRequest(savedAccountList: AccountList, accountList: AccountList): AccountList {
-    // TODO domainがreaderに依存すな
-    const accountListByToList: AccountList = AccountDomReader.getAccountList()
-    /**
-     * チャット外の人は使い回す
-     * 既にGroupに追加されている人は使い回さない（名前とか変わっている可能性あるので）
-     * @type {Array<Account>}
-     */
-    const reuseAccountListAccount = savedAccountList.value.filter((oldAccount) => {
-      const isOutsider = !accountListByToList.value.some(
-        (toAccount) => toAccount.accountId === oldAccount.accountId
-      )
-      const exists = accountList.value.some(
-        (reqAccount) => reqAccount.accountId !== oldAccount.accountId
-      )
-
-      return isOutsider || !exists
-    })
-
-    return { value: reuseAccountListAccount.concat(accountList.value) }
-  },
 }
